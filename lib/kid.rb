492dd0140fa226a923c4dce78e8445ab88495767
Patrick Kim <pkim050@gmail.com>
2019-03-02 00:31:06 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-  include Dance
+  include FancyDanceDance
   extend MetaDancing
  
   attr_accessor :name
